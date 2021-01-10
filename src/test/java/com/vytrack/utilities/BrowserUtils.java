3e1a65d3a7ba79b55a59ecba7b257a41d4a7fa06
pkouliev <pkouliev@gmail.com>
2021-01-10 00:35:29 -0500

    space method added as part BrowserUtils class

---
+++ src/test/java/com/vytrack/utilities/BrowserUtils.java
@@ -0,0 +1,9 @@
+package com.vytrack.utilities;
+
+public class BrowserUtils {
+
+    public static void space() {
+
+        System.out.println("\n================================================================================================\n");
+    }
+}
