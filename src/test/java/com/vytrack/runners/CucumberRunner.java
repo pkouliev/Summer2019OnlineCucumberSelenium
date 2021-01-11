4b8eeb4b2cf491b27779f5bef40c0bd709134bca
pkouliev <pkouliev@gmail.com>
2021-01-10 21:05:03 -0500

    defaul cucumber report plugin added

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@negative_test"
+        tags = "",
+        plugin = {"html:target/default-cucumber-reports"}
 )
 public class CucumberRunner {
 
