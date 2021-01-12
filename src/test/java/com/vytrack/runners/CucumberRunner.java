ded59db865fc3027ecc7e3b8f7514b1fc48e3ff0
pkouliev <pkouliev@gmail.com>
2021-01-11 19:08:01 -0500

    used "smoke_test" tag to run new feature with this specific tag

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@store_manager",
+        tags = "@smoke_test",
         plugin = {"html:target/default-cucumber-reports.html"}
 )
 public class CucumberRunner {
