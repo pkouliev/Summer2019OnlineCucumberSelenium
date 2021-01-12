17adb116aa614756654e25665b85da6470620dec
pkouliev <pkouliev@gmail.com>
2021-01-12 08:31:33 -0500

    Added TakesScreenshot() interface method by casting to use attach() method for grtting image once test failed

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -5,6 +5,8 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
 
@@ -20,6 +22,8 @@ public class Hook {
         // if test failed - do this
         if (scenario.isFailed()) {
             System.out.println("Test failed");
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Failed Test Screenshot");
         } else {
             System.out.println("Cleanup!");
             System.out.println("Test completed!");
