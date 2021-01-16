f24affa86c2461ce6247f067b986fafecc3faf1a
pkouliev <pkouliev@gmail.com>
2021-01-16 15:42:36 -0500

    Driver utulity updated with new setup options

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -2,60 +2,76 @@ package com.vytrack.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
-    // you cannot do like this, if constructor is private Driver obj = new Driver()
-//    private Driver() {
-//
-//    }
+    private Driver() {
 
-    // if switch statement complains on String parameter
-    // change java version to the latest.
-    // File -> Project Structure -> Set Project language level to the latest
+    }
+
     public static WebDriver getDriver() {
-        // if WebDriver object was not created yet
-        if (driver == null) {
-            // create WebDriver object based on browser value from properties
-            String browser = ConfigurationReader.getValue("browser");
-            // if browser type is wrong, stop tests and throw exception
-            // no browser will be opened
+        //if this thread doesn't have a web driver yet - create it and add to pool
+        if (driverPool.get() == null) {
+            System.out.println("TRYING TO CREATE DRIVER");
+            // this line will tell which browser should open based on the value from properties file
+            String browserParamFromEnv = System.getProperty("browser");
+            String browser = browserParamFromEnv == null ? ConfigurationReader.getValue("browser") : browserParamFromEnv;
             switch (browser) {
-                case "chrome" -> {
+                case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    // to configure chrome browser for tests
-                    driver = new ChromeDriver();
-                }
-                case "chrome_headless" -> {
+                    driverPool.set(new ChromeDriver());
+                    break;
+                case "chrome_headless":
                     WebDriverManager.chromedriver().setup();
-                    // to configure chrome browser for tests
-                    ChromeOptions options = new ChromeOptions();
-                    // to run tests without interface, set to true
-                    options.setHeadless(true);
-                    driver = new ChromeDriver(options);
-                }
-                case "firefox" -> {
+                    driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
+                    break;
+                case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                }
-                default -> throw new RuntimeException("Wrong browser type");
+                    driverPool.set(new FirefoxDriver());
+                    break;
+                case "firefox_headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                    break;
+                case "ie":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    }
+                    WebDriverManager.iedriver().setup();
+                    driverPool.set(new InternetExplorerDriver());
+                    break;
+                case "edge":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driverPool.set(new EdgeDriver());
+                    break;
+                case "safari":
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your OS doesn't support Safari");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driverPool.set(new SafariDriver());
+                    break;
             }
+
         }
-        // if WebDriver object was created - you can use it
-        return driver;
+        //return corresponded to thread id webdriver object
+        return driverPool.get();
     }
 
     public static void close() {
-        // if driver still exists
-        if (driver != null) {
-            // close all browsers
-            driver.quit();
-            // destroy driver object, ready for garbage collector (gc)
-            driver = null;
-        }
+        driverPool.get().quit();
+        driverPool.remove();
     }
 }
